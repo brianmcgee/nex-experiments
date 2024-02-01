9acb9ee85218d13098ff173523a85b2015a04d2d
Brian McGee <brian@bmcgee.ie>
2024-02-01 17:13:35 +0000

    feat: add a dev util for cleaning up veths

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -21,6 +21,19 @@
       commands = [
         {package = pkgs.gomod2nix;}
         {package = inputs'.flake-linter.packages.default;}
+
+        {
+          package = pkgs.writeShellApplication {
+            name = "clean";
+            text = ''
+              for name in $(ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d' | grep veth)
+              do
+                  echo "$name"
+                  sudo ip link delete "$name"
+              done
+            '';
+          };
+        }
       ];
     };
   };
